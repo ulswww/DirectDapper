2c112a5ee88a067176645bccf29ef996750c8ece
ulswww <ulswww@gmail.com>
2020-08-23 11:28:20 +0800

    修改测试用例

--- Tests/DirectDapper.Tests/EF_Tests.cs
+++ Tests/DirectDapper.Tests/EF_Tests.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
+using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
 using DirectDapper.Providers;
 using DirectDapper.Tests.Domain;
+using Shouldly;
 using Xunit;
 
 namespace DirectDapper.Tests
@@ -12,13 +14,22 @@ namespace DirectDapper.Tests
         public async Task GetSql_test()
         {
             var unitOfWork = Resolve<IUnitOfWorkManager>();
+            var ticketRep = Resolve<IRepository<Ticket>>();
             using (var uow = unitOfWork.Begin())
             {
+                await ticketRep.InsertAsync(new Ticket());
+                await ticketRep.InsertAsync(new Ticket());
+
+                await  unitOfWork.Current.SaveChangesAsync();
+
+                var adapter =  Resolve<IDirectDapperQueryProvider>()
+                                               .GetSimpleQueryAdapter("Sqls.Hello.GetWorld2.s"); 
+
                 var s = await Resolve<IDirectDapperQueryProvider>()
                                                .GetSimpleQueryAdapter("Sqls.Hello.GetWorld2.s")
                                                .SetQueryObj(new { })
                                                .GetResultsAsync<Ticket>();
-
+                s.Count.ShouldBe(2);
             }
         }
     }
